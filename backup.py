b6000e43b3ab18c4e1b508a75cefb59323dcf977
boykjgis <boykjgis@gmail.com>
2023-07-25 14:46:56 -0600

    backup.py: Install dotenv and import

--- backup.py
+++ backup.py
@@ -1,11 +1,14 @@
 from datetime import *
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 label = datetime.now().strftime('%Y-%m-%d_Time-%H-%M')
 PW = os.getenv('ARCGIS_PW')
 
 def main():
-    print(f'Hello at {label})
+    print(f'Hello at {label}')
 
 if __name__ == '__main__':
     main()
