cde63b52bb496e7d3d3da91ec92a0eaabd1efb0f
boykjgis <boykjgis@gmail.com>
2023-07-25 14:51:53 -0600

    backup.py: Add backup_path constant

--- backup.py
+++ backup.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 label = datetime.now().strftime('%Y-%m-%d_Time-%H-%M')
 PW = os.getenv('ARCGIS_PW')
+BACKUP_PATH = os.getenv('BACKUP_PATH')
 
 def main():
     print(f'Hello at {label}')
