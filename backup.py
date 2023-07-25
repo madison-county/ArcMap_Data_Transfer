673afab9a8c1cba0a98f8f252ba57902a2b48a5d
boykjgis <boykjgis@gmail.com>
2023-07-25 15:09:54 -0600

    backup.py: Fix pathing issues with dir creation

--- backup.py
+++ backup.py
@@ -1,17 +1,23 @@
 import os
+import shutil
 from datetime import *
 from dotenv import load_dotenv
 
 load_dotenv()
 
-label = datetime.now().strftime('%Y-%m-%d_')
-PW = os.getenv('ARCGIS_PW')
-BACKUP_PATH = os.getenv('BACKUP_PATH')
+BACKUP_PATH = r'//madco-gnas/GIS-SSD/01_Working-Data/04_Backup/02-Data/2023_Backup-Broad'
 
-FULL_PATH = BACKUP_PATH + label + 'Scripted-Backup'
+current_date = datetime.now().strftime('%Y-%m-%d_')
+PW = os.getenv('ARCGIS_PW')
+#BACKUP_PATH = os.getenv('BACKUP_PATH')
+
+label = current_date + 'Scripted-Backup/'
+
+FULL_PATH = os.path.join(BACKUP_PATH, label)
 
 def main():
     print(f'{label, FULL_PATH}')
+    os.mkdir(FULL_PATH)
 
 if __name__ == '__main__':
     main()
